9a69ec8bcd53c454e7b95b1b67fb820a9c087806
Baetaekyung <tkwayi2129@gmail.com>
2024-11-23 15:06:48 +0900

    UI Images

---
+++ Assets/_Script/UI/UIManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[MonoSingletonUsage(MonoSingletonFlags.DontDestroyOnLoad)]
+public class UIManager : MonoSingleton<UIManager>
+{
+    
+}
