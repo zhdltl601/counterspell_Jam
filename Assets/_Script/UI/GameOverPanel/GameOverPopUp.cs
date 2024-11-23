13857d98946f81dc9f9863462afae7e24b205e1f
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-11-23 21:15:02 +0900

    Add Torch And Dead System

--- Assets/_Script/UI/GameOverPanel/GameOverPopUp.cs
+++ Assets/_Script/UI/GameOverPanel/GameOverPopUp.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +10,8 @@ public class GameOverPopUp : MonoBehaviour, IPopUpable
 {
     private CanvasGroup _gameOverPanelGroup;
     [SerializeField] private Image backGround;
-    
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private DeadDescriptinSO DescriptinSo;
     private void Awake()
     {
         _gameOverPanelGroup = GetComponent<CanvasGroup>();
@@ -17,6 +19,8 @@ public class GameOverPopUp : MonoBehaviour, IPopUpable
 
     public void PopUp()
     {
+        gameOverText.SetText(DescriptinSo.GetDescription());
+        
         backGround.DOFade(0.7f , 0.7f).OnComplete(() =>
         {
             _gameOverPanelGroup.DOFade(1, 0.3f);
